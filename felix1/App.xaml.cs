0654f033ac965915895e9e966ab7ceaa072f4aa6
firnex <fernandorodriguezb27@gmail.com>
2025-05-01 13:15:38 -0400

    adding syncfusion

--- felix1/App.xaml.cs
+++ felix1/App.xaml.cs
@@ -4,6 +4,8 @@ public partial class App : Application
 {
 	public App()
 	{
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NDaF5cWGBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWH5ccnRcRmVfUUR0XEU=");
+
 		InitializeComponent();
 
 		MainPage = new AppShell();
