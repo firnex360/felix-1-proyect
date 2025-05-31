f0aa12f4f5e59e019efeba672d6c55096b3a80fb
firnex <fernandorodriguezb27@gmail.com>
2025-05-31 15:34:10 -0400

    FLX-24 Created and Implemented database
    for more details please refer the general doc.
    
    in summary will be using EF Core (for ORM) and SQLite (for database engine)

--- felix1/MainPage.xaml.cs
+++ felix1/MainPage.xaml.cs
@@ -1,31 +1,52 @@
-﻿namespace felix1;
+﻿using felix1.Data;
+using felix1.Logic;
+namespace felix1;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    int count = 0;
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 
 
     private async void OnGoToPageBClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Example()); //CHECKING - navigate to example page
     }
+
+    private void OnSaveUserTest(object sender, EventArgs e)
+    {
+        using var db = new AppDbContext();
+        
+        var newUser = new User
+        {
+            Name = "John Doe",
+            Username = "johnd",
+            Password = "secret123",
+            Role = "Admin"
+        };
+
+        db.Users.Add(newUser);
+        db.SaveChanges();
+
+    }
+    
 
 }
 
