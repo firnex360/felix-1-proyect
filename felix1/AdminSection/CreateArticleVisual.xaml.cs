5d0d4aad228c72bff636e6b18573aede7043709f
mari <mita0001@ce.pucmm.edu.do>
2025-06-05 18:58:17 -0400

    FLX52 || CREATE for Article functional.

--- felix1/AdminSection/CreateArticleVisual.xaml.cs
+++ felix1/AdminSection/CreateArticleVisual.xaml.cs
@@ -1,3 +1,6 @@
+using felix1.Data;
+using felix1.Logic;
+
 namespace felix1;
 
 public partial class CreateArticleVisual : ContentView
@@ -5,8 +8,15 @@ public partial class CreateArticleVisual : ContentView
 	public CreateArticleVisual()
 	{
 		InitializeComponent();
+
+		
+        // Populate Picker with enum values
+        pckCategory.ItemsSource = Enum.GetValues(typeof(ArticleCategory))
+                                         .Cast<ArticleCategory>()
+                                         .Select(c => c.ToString())
+                                         .ToList();
 	}
-	
+
 	private void OnShowA(object sender, EventArgs e)
 	{
 		RightPanelA.IsVisible = true;
@@ -17,5 +27,26 @@ public partial class CreateArticleVisual : ContentView
 	{
 		RightPanelA.IsVisible = false;
 		RightPanelB.IsVisible = true;
+	}
+
+	private void OnSaveArticle(object sender, EventArgs e)
+	{
+		using var db = new AppDbContext();
+
+        var selectedCategory = pckCategory.SelectedItem?.ToString();
+        var parsed = Enum.TryParse<ArticleCategory>(selectedCategory, out var categoryEnum);
+
+		var newArticle = new Article
+		{
+			Name = txtName.Text,
+			PriPrice = txtPrice.Text != null ? float.Parse(txtPrice.Text) : 0f,
+			SecPrice = txtSecondaryPrice.Text != null ? float.Parse(txtSecondaryPrice.Text) : 0f,
+			Category = parsed ? categoryEnum : ArticleCategory.Other,
+			IsDeleted = false,
+			IsSideDish = txtSideDish.IsChecked,
+		};
+
+		db.Articles.Add(newArticle);
+		db.SaveChanges();
 	}
 }
