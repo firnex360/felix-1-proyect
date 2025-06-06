ea24633944fb2dc3f979135e27519105495af7bf
firnex <fernandorodriguezb27@gmail.com>
2025-06-06 12:52:20 -0400

    still stable version

--- felix1/AdminSection/ListArticleVisual.xaml.cs
+++ felix1/AdminSection/ListArticleVisual.xaml.cs
@@ -28,10 +28,29 @@ public partial class ListArticleVisual : ContentView
         foreach (var article in articlesFromDb)
             Articles.Add(article);
     }
-    
+
     private void OnCreateArticleClicked(object sender, EventArgs e)
     {
-        labeltest.Text = "Button clicked";
+        // var window = new Window(new CreateArticleVisual()); // or MainPage()
+        // return window;
+    }
+
+    private void OnViewClicked(object sender, EventArgs e)
+    {
+        // Dummy function for View button
+        labeltest.Text = "View clicked";
+    }
+
+    private void OnEditClicked(object sender, EventArgs e)
+    {
+        // Dummy function for Edit button
+        labeltest.Text = "Edit clicked";
+    }
+
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        // Dummy function for Delete button
+        labeltest.Text = "Delete clicked";
     }
 
 }
