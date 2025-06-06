82c779f8636b7da5edf18bead3bea67e36ce8d3a
firnex <fernandorodriguezb27@gmail.com>
2025-06-06 12:28:23 -0400

    data grid working

--- felix1/AdminSection/ListArticleVisual.xaml.cs
+++ felix1/AdminSection/ListArticleVisual.xaml.cs
@@ -1,11 +1,32 @@
+using System.Collections.ObjectModel;
+using felix1.Data;
+using felix1.Logic;
+using Microsoft.EntityFrameworkCore;
+
 namespace felix1;
 
 public partial class ListArticleVisual : ContentView
 {
+
+    public ObservableCollection<Article> Articles { get; set; } = new();
+
     public ListArticleVisual()
     {
         InitializeComponent();
+        BindingContext = this;
+        LoadArticles();
+    }
 
+    private void LoadArticles()
+    {
+        using var db = new AppDbContext();
+        var articlesFromDb = db.Articles
+            .Where(a => !a.IsDeleted)
+            .ToList();
+
+        Articles.Clear();
+        foreach (var article in articlesFromDb)
+            Articles.Add(article);
     }
     
     private void OnCreateArticleClicked(object sender, EventArgs e)
