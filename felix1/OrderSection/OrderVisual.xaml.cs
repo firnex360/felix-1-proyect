e48e06c8345d879041480c466c66affb0f4c1812
firnex <fernandorodriguezb27@gmail.com>
2025-07-03 18:14:34 -0400

    added tables to ordervisual

--- felix1/OrderSection/OrderVisual.xaml.cs
+++ felix1/OrderSection/OrderVisual.xaml.cs
@@ -1,9 +1,59 @@
+using System.Collections.ObjectModel;
+using felix1.Logic;
+using felix1.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace felix1.OrderSection;
 
 public partial class OrderVisual : ContentPage
 {
-	public OrderVisual()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Article> ListArticles { get; set; } = new();
+    public ObservableCollection<OrderItemDisplay> OrderItems { get; set; } = new();
+
+    // Helper class for DataGrid display
+    public class OrderItemDisplay
+    {
+        public int Quantity { get; set; }
+        public string ArticleName { get; set; } = "";
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
+    }
+
+    private void LoadArticles()
+    {
+        var articlesFromDb = AppDbContext.ExecuteSafeAsync(async db =>
+            await db.Articles
+                .Where(a => !a.IsDeleted)
+                .ToListAsync())
+            .GetAwaiter().GetResult();
+
+        ListArticles.Clear();
+        foreach (var article in articlesFromDb)
+            ListArticles.Add(article);
+    }
+
+    private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var searchText = e.NewTextValue?.ToLower() ?? "";
+
+        if (string.IsNullOrWhiteSpace(searchText))
+        {
+            // Reset the DataGrid to show all articles
+            listArticleDataGrid.ItemsSource = ListArticles;
+        }
+        else
+        {
+            // Filter the collection
+            listArticleDataGrid.ItemsSource = ListArticles
+                .Where(a => a.Name != null && a.Name.ToLower().Contains(searchText))
+                .ToList();
+        }
+    }
+
+    public OrderVisual()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        LoadArticles();
+    }
 }
