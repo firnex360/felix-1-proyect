0a38529e2220dda6efa5d098c711cbfc99c82b91
firnex <fernandorodriguezb27@gmail.com>
2025-07-03 21:53:48 -0400

    some more orderitem class change

--- felix1/Logic/OrderItem.cs
+++ felix1/Logic/OrderItem.cs
@@ -1,15 +1,43 @@
 using System;
+using System.ComponentModel;
 
 namespace felix1.Logic;
 
-public class OrderItem
+public class OrderItem : INotifyPropertyChanged
 {
     public int Id { get; set; } // Auto-generated in database
     public Article? Article { get; set; } = null;
-    public int Quantity { get; set; } = 0; 
-    public decimal UnitPrice { get; set; } = 0;
+    
+    private int _quantity = 0;
+    public int Quantity 
+    { 
+        get => _quantity;
+        set 
+        {
+            _quantity = value;
+            OnPropertyChanged(nameof(TotalPrice));
+        }
+    }
+    
+    private decimal _unitPrice = 0;
+    public decimal UnitPrice 
+    { 
+        get => _unitPrice;
+        set 
+        {
+            _unitPrice = value;
+            OnPropertyChanged(nameof(TotalPrice));
+        }
+    }
     
     // Properties for DataGrid binding
     public string ArticleName => Article?.Name ?? "";
     public decimal TotalPrice => Quantity * UnitPrice;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
