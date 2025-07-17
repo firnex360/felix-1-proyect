234dc7397113630463186543d1ba3cdecf4ce56a
firnex <fernandorodriguezb27@gmail.com>
2025-07-17 12:19:46 -0400

    FLX-67 & FLX-166, can switch between local tables
    for more commit for FLX-67, check subtasks: FLX-122, FLX-166, FLX-167 and FLX-123

--- felix1/OrderSection/OrderSectionMainVisual.xaml.cs
+++ felix1/OrderSection/OrderSectionMainVisual.xaml.cs
@@ -2,6 +2,11 @@ using felix1.Data;
 using felix1.Logic;
 using Microsoft.Maui.ApplicationModel;
 
+#if WINDOWS
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml;
+#endif
+
 
 namespace felix1.OrderSection;
 
@@ -17,7 +22,7 @@ public partial class OrderSectionMainVisual : ContentPage
         RightPanel.Content = new ListOrderVisual();
 
 #if WINDOWS
-        var window = Application.Current?.Windows.FirstOrDefault();
+        var window = Microsoft.Maui.Controls.Application.Current?.Windows.FirstOrDefault();
         if (window != null)
         {
             WindowUtils.MaximizeWindow(window);
@@ -65,7 +70,7 @@ public partial class OrderSectionMainVisual : ContentPage
                 Application.Current!.MainPage = new NavigationPage(balanceVisual);*/
                 Navigation.PopAsync();
                 var balanceVisual = new LoginPage();
-                Application.Current!.MainPage = new NavigationPage(balanceVisual);
+                Microsoft.Maui.Controls.Application.Current!.MainPage = new NavigationPage(balanceVisual);
             });
         });
     }
@@ -84,7 +89,7 @@ public partial class OrderSectionMainVisual : ContentPage
         AppSession.CurrentUser = null!;
 
         var loginPage = new LoginPage();
-        Application.Current!.MainPage = new NavigationPage(loginPage);
+        Microsoft.Maui.Controls.Application.Current!.MainPage = new NavigationPage(loginPage);
     }
 
     [Obsolete("This method is obsolete, delete this if sure wont be of use in the future")]
@@ -121,7 +126,7 @@ public partial class OrderSectionMainVisual : ContentPage
         }
         else
         {
-            await Application.Current!.MainPage!.DisplayAlert("Error", "No se puede navegar a la página de pago", "OK");
+            await Microsoft.Maui.Controls.Application.Current!.MainPage!.DisplayAlert("Error", "No se puede navegar a la página de pago", "OK");
         }
     }
 
@@ -155,4 +160,41 @@ public partial class OrderSectionMainVisual : ContentPage
             searchBar?.ClearValue(Entry.TextProperty); // Clear the search bar text
         });
     }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        this.HandlerChanged += OnHandlerChanged;
+        OnHandlerChanged(this, EventArgs.Empty);
+    }
+
+    private void OnHandlerChanged(object? sender, EventArgs e)
+    {
+#if WINDOWS
+        if (searchBar?.Handler?.PlatformView is Microsoft.UI.Xaml.Controls.AutoSuggestBox autoSuggestBox)
+        {
+            autoSuggestBox.KeyDown -= SearchBarPlatformView_KeyDown;
+            autoSuggestBox.KeyDown += SearchBarPlatformView_KeyDown;
+        }
+#endif
+    }
+
+#if WINDOWS
+    private void SearchBarPlatformView_KeyDown(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Tab)
+        {
+            if (RightPanel.Content is ListOrderVisual listOrderVisual)
+            {
+                listOrderVisual.MoveToNextMatchingTable();
+                e.Handled = true;
+            }
+        }
+        else if (e.Key == Windows.System.VirtualKey.Enter)
+        {
+            OnSearchBarSearchButtonPressed(sender, EventArgs.Empty);
+            e.Handled = true;
+        }
+    }
+#endif
 }
